fa2fc36d43f2b11cf502ac80c68afe82257a0231
Benjamin J Phillips <bephillips2@gmail.com>
2017-08-02 00:44:29 -0700

    Fixed method used to compute Beta and Intercept via Cross Validation

--- glmnet_regression.py
+++ glmnet_regression.py
@@ -13,22 +13,20 @@ class multiCV:
 
         col_names = list( raw_data.columns.values )
 
-        Y = Y.as_matrix()
-        X = X.as_matrix()
+        Y = Y.as_matrix().astype(np.float64)
+        X = X.as_matrix().astype(np.float64)
 
-        fit = glmnet_py.cvglmnet( x = X, y = Y )["glmnet_fit"]
+        fit = glmnet_py.cvglmnet( x = X.copy(), y = Y.copy() )
 
-        #Default parameters seem to return 2D array for beta and 1D array for a0
-        # It seems that the 'first' element is the Beta and Intercept that we expect?
-        coefficients = fit["beta"][:,1]
-        intercept = fit["a0"][:1]
+        coefficients = glmnet_py.cvglmnetCoef( fit, s = 'lambda_min')
 
         nz_indices = coefficients.nonzero()[0]
         support_coefs = coefficients[ nz_indices ]
-        col_names = [ col_names[idx] for idx in nz_indices ]
 
+        # cvglmnetCoef places the intercept term in the first position of the
+        # coefficients vector -- need to manually add Intercept name to  vector of column names
         col_names.insert( 0, "Intercept" )
-        support_coefs = np.insert( support_coefs, 0, intercept )
+        col_names = [ col_names[idx] for idx in nz_indices ]
 
         return( pd.DataFrame( data = support_coefs , index = col_names ).to_json(orient='columns') )
 
